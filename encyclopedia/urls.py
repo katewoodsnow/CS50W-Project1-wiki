e6bdb2bed4813ec0e09e1c5093df9b85c2061fd0
katewoodsnow <kate.wood@hotmail.co.uk>
2020-07-08 21:55:31 +0100

    index page links

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 # A list of all the allowable urls for the app
+
+# uniquely identify all the urls to the app
+app_name = "encyclopedia"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.wiki, name = "wiki"),
+    path("wiki/<str:title>", views.entry, name ="entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,6 +5,7 @@ from . import util
 # views file decides what the user sees when they visit a particular route
 # functions are used for the view. Request is the http request made by the user in order to access the web server
 # tell the app what url the user is going to visit in order to return the response in the function
+
 # tell Django when the url is visited then this function should be run in able to return the list of entries in the
 # encyclopedia
 def index(request):
@@ -12,10 +13,19 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def wiki(request, title):
-    return render(request, "encyclopedia/wiki.html", {
-        "contents": util. get_entry(title)
-    })
+# Displays the contents of the subject/title searched for
+def entry(request, title):
+    content = util.get_entry(title)
+    if not content:
+        return render(request, "encyclopedia/error.html", {
+            "message": "Sorry, this entry does not exist"
+        })
+        
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "content": content,
+            "title": title,
+        })
 
 
 
